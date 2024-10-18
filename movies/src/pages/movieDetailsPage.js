a18afee3205c6aae9af93586726fc3d705727db3
zft666zft <2810458566@qq.com>
2024-10-18 14:20:18 +0100

    Show movie review excerpts; Refactor Details page.

--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -1,50 +1,31 @@
 import React, {useState, useEffect}  from "react";
 import { useParams } from 'react-router-dom';
-import MovieHeader from "../components/headerMovie/";
 import MovieDetails from "../components/movieDetails/";
-import Grid from "@mui/material/Grid2";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import { getMovie, getMovieImages } from "../api/tmdb-api";
+import PageTemplate from "../components/templateMoviePage";
+import { getMovie } from "../api/tmdb-api";
 
 const MoviePage = (props) => {
-    const { id } = useParams();
-    const [movie, setMovie] = useState(null);
-    const [images, setImages] = useState([]);
-  
-    useEffect(() => {
-        getMovie(id).then((movie) => {
-          setMovie(movie);
-        });
-      }, [id]);
-    
-      useEffect(() => {
-        getMovieImages(id).then((images) => {
-          setImages(images);
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    getMovie(id).then((movie) => {
+      setMovie(movie);
+    });
+  }, [id]);
 
   return (
-    <ImageList
-    sx={{
-      height: "100vh",
-    }}
-    cols={1}
-  >
-    {images.map((image) => (
-      <ImageListItem
-      key={image.file_path}
-      cols={1}
-      >
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-          alt={image.file_path}
-        />
-      </ImageListItem>
-    ))}
-  </ImageList>
-  
+    <>
+      {movie ? (
+        <>
+          <PageTemplate movie={movie}>
+            <MovieDetails movie={movie} />
+          </PageTemplate>
+        </>
+      ) : (
+        <p>Waiting for movie details</p>
+      )}
+    </>
   );
 };
 

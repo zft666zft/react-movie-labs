474c9a589ec4b5ccdbebb51ce127794c7cb267d2
zft666zft <2810458566@qq.com>
2024-10-18 13:30:18 +0100

    Movie Details Page rendering.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/homePage";
+import MovieDetailsPage from './pages/movieDetailsPage'
 
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
 const sample = {
   adult: false,
   backdrop_path: "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
@@ -87,7 +94,7 @@ const movies = [sample, sample, sample, sample, sample, sample, sample];
 
 const App = () => {
   return (
-      <HomePage movies={movies} />
+    <MovieDetailsPage movie={sample} images={images} />
   );
 };
 

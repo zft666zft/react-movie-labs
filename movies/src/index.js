3e180933670bf4ab8d67b58461b3bced41e7a436
zft666zft <2810458566@qq.com>
2024-11-08 21:24:00 +0000

    Added new review support.

--- movies/src/index.js
+++ movies/src/index.js
@@ -9,6 +9,7 @@ import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 
 
 const queryClient = new QueryClient({
@@ -33,6 +34,8 @@ const App = () => {
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={ <Navigate to="/" /> } />
+            <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
+
           </Routes>
         </MoviesContextProvider>
       </BrowserRouter>

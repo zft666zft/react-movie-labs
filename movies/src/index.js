b1eebcf735058d551322c26662b580aee4024f2b
zft666zft <2810458566@qq.com>
2024-11-08 19:38:44 +0000

    Movie Details Page rendering.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,7 +1,15 @@
+import MovieDetailsPage from './pages/movieDetailsPage'
 import React from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/homePage";
 
+
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
 const sample = {
   adult: false,
   backdrop_path: "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
@@ -87,8 +95,8 @@ const movies = [sample, sample, sample, sample, sample, sample, sample];
 
 const App = () => {
   return (
-      <HomePage movies={movies} />
-  );
+      <MovieDetailsPage movie={sample} images={images} />
+      );
 };
 
 const rootElement = createRoot( document.getElementById("root") )

0a885ee5b8292a2e613bdfba0290165e2d76a043
zft666zft <2810458566@qq.com>
2024-11-10 22:34:31 +0000

    add link to actors and crews and click on the link to jump to a film related to him/her

--- movies/src/pages/movieCreditsPage.js
+++ movies/src/pages/movieCreditsPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
 import { getMovieCredits } from "../api/tmdb-api";
@@ -31,7 +31,11 @@ const MovieCreditsPage = () => {
               style={{ borderRadius: "50%", marginRight: "15px" }}
             />
             <div>
-              <strong style={{ fontSize: '18px', color: '#3f51b5' }}>{actor.name}</strong> as {actor.character}
+              {/* 演员名字的链接 */}
+              <Link to={`/actor/${actor.id}/movies`} style={{ fontSize: '18px', color: '#3f51b5' }}>
+                {actor.name}
+              </Link>
+              <p style={{ margin: "5px 0 0", color: "#757575" }}>as {actor.character}</p>
             </div>
           </li>
         ))}
@@ -47,7 +51,11 @@ const MovieCreditsPage = () => {
               style={{ borderRadius: "50%", marginRight: "15px" }}
             />
             <div>
-              <strong style={{ fontSize: '18px', color: '#3f51b5' }}>{member.name}</strong> - {member.job}
+              {/* 制作人员名字的链接 */}
+              <Link to={`/actor/${member.id}/movies`} style={{ fontSize: '18px', color: '#3f51b5' }}>
+                {member.name}
+              </Link>
+              <p style={{ margin: "5px 0 0", color: "#757575" }}>{member.job}</p>
             </div>
           </li>
         ))}

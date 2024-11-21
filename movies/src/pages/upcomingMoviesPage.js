0a7baa1d5a825b57a4f9fd722102900b62284cb1
zft666zft <2810458566@qq.com>
2024-11-21 15:28:14 +0000

    Make some changes on the upcomingMoviesPage.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -3,12 +3,11 @@ import { useQuery } from "react-query";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import PageTemplate from "../components/templateMovieListPage";
 import Spinner from "../components/spinner";
-import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
-import { MoviesContext } from "../contexts/moviesContext";
+import AddToWatchlistIcon from "../components/cardIcons/addToWatchlist";
+import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
 
 const UpcomingMoviesPage = () => {
   const { data, error, isLoading, isError } = useQuery("upcoming", getUpcomingMovies);
-  const { mustWatch, addToMustWatch } = useContext(MoviesContext);
 
   if (isLoading) {
     return <Spinner />;
@@ -25,11 +24,10 @@ const UpcomingMoviesPage = () => {
       title="Upcoming Movies"
       movies={movies}
       action={(movie) => (
-        <PlaylistAddIcon
-          color={mustWatch.includes(movie.id) ? "secondary" : "primary"} // 根据 mustWatch 状态改变颜色
-          sx={{ fontSize: 30, cursor: "pointer" }}
-          onClick={() => addToMustWatch(movie.id)} // 绑定点击事件
-        />
+        <>
+          <AddToFavoritesIcon movie={movie} />
+          <AddToWatchlistIcon movie={movie} />
+        </>
       )}
     />
   );
